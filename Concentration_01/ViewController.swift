5d286c08a236fc54021d59e5c2467198272df818
agoodis <agoodis@gmail.com>
2018-03-26 13:42:49 +0300

    Initial Commit

---
+++ Concentration_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration_01
+//
+//  Created by Андрей Гранди on 26/03/2018.
+//  Copyright © 2018 Андрей Гранди. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
